023978f8477dce7693fe3232451f409aa16e25d6
grnsv <grin.sv@gmail.com>
2022-04-15 15:45:42 +0300

    mix files included

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@section('title'){{ config('app.name', 'CitiesOfRussia') }}@show</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
