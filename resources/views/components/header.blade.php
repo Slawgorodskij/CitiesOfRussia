7f52d0ca8e5e9bc95450b313a127d3a39c1e66c6
grnsv <grin.sv@gmail.com>
2022-04-16 13:07:57 +0300

    header updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,19 @@
-<header>
-    <h3>Меню</h3>
-    <ul>
-        <li>
-            <a href="{{ route('index') }}">Главная</a>
-        </li>
-        <li>
-            <a href="{{ route('admin.index') }}">В админку</a>
-        </li>
-    </ul>
+<header class="header">
+    <a href="{{ route('index') }}">
+        <img src="/storage/images/logo.png" alt="logo">
+    </a>
+    <nav>
+        <button class="burger" onclick="this.classList.toggle('burger--active');
+        menu.classList.toggle('header__menu--opened')">
+            <div class="burger__line"></div>
+        </button>
+        <ul class="header__menu" id="menu">
+            <li class="header__menu_item">
+                <a href="{{ route('index') }}" class="header__menu_link">Главная</a>
+            </li>
+            <li class="header__menu_item">
+                <a href="{{ route('admin.index') }}" class="header__menu_link">В админку</a>
+            </li>
+        </ul>
+    </nav>
 </header>
