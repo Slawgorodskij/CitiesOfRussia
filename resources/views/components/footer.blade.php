de4a73f88167623c88baef6e1d8ad056c54c7b3d
grnsv <grin.sv@gmail.com>
2022-05-10 21:29:21 +0300

    fix

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="footer__block container ">
         <a href="{{ route('index') }}">
-            <img src="/storage/images/logo.png" alt="logo">
+            <img src="/images/logo.png" alt="logo">
         </a>
         <p>Young Programmers © 2022</p>
         <a class="footer__link" href="#">Связаться с нами</a>
