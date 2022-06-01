48f794d776a52b3490a28f98be1a49a26be35369
Slawgorodskij <Slawgorodskij@mail.ru>
2022-06-01 23:24:10 +0700

    correction

--- app/Http/Controllers/SightController.php
+++ app/Http/Controllers/SightController.php
@@ -5,17 +5,19 @@ namespace App\Http\Controllers;
 use App\Models\City;
 use App\Models\Profile;
 use App\Models\Sight;
+use Illuminate\Support\Facades\Auth;
 
 class SightController extends Controller
 {
     public function index($citySlug, $sightSlug)
     {
+        $arrayComments = [];
         $city = City::with('sights')->where('slug', $citySlug)
             ->first();
         $sight = Sight::with('images', 'articles')->where('slug', $sightSlug)
             ->first();
+        $profile = Auth::user()->profiles;
 
-        $arrayComments = [];
 
         foreach ($sight->comments as $commentItem) {
             $user = Profile::where('user_id', $commentItem->user_id)->first();
@@ -32,7 +34,8 @@ class SightController extends Controller
             'sights' => $city->sights,
             'comments' => $comments,
             'cityId' => $city->id,
-            'cityName' => $city->name,
+            'cityArrivalName' => $city->name,
+            'departureCityName' => $profile[0]['city'],
             'citySlug' => $city->slug,
         ]);
     }
